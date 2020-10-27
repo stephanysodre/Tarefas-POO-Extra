366c3546fde4a492c64e0e29be49d8d3393dfa07
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-10-27 20:02:02 -0300

    atividade final exemplo

---
+++ Tarefa-POO-Exemplo/Tarefa-POO-Exemplo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarefa_POO_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("Fábio");
+            list.Add("Wilson");
+            list.Add("Angelina");
+            list.Add("Eliney");
+            list.Insert(2, "Lucas");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("List count: " + list.Count);
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("First 'A': " + s1);
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First position 'A': " + pos1);
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last position 'A': " + pos2);
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("---------------------");
+            foreach (string obj in list2)
+            {
+                Console.WriteLine(obj);
+            }
+            list.Remove("Alex");
+            Console.WriteLine("---------------------");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("---------------------");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
