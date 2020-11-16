5bb88cd28c8b3431b09ccfa0db3d42c72258ddad
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-11-16 10:17:13 -0300

    Update Class1.cs

--- Tarefa Herança/Class1.cs
+++ Tarefa Herança/Class1.cs
@@ -5,31 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Tarefa_Herança
-//Criado dentro do projeto uma pasta entidades para separar classes do programa principal
-{
-	//Criação da classe Conta
-	class Conta
-	{
-		//Declarações das variáveis e uso do encapsulamento
-		public int Numero { get; privateset; }
-		public string Titular { get; privateset; }
-		//Utilização do protected para alteração de valores entre classes
-		public double Saldo { get; protectedset; }
-		//Criação do construtor padrão e construtor com parâmetros
-		public Conta() { }
-		public Conta(intnumero, stringtitular, doublesaldo)
-		{
-			Numero = numero; Titular = titular; Saldo = saldo;
+namespaceCourse.Entities{
+class ContaJuridica: Conta {
+		public double EmprestimoLimite{ get; set; }
+		public ContaJuridica() {}
+		public ContaJuridica
+		(intnumero, stringtitular, doublesaldo, doubleemprestimoLimite)
+		: base(numero, titular, saldo){
+			EmprestimoLimite = emprestimoLimite;
+			}
+			public void Emprestimo(doublesaldoTotal){
+				if (saldoTotal<= EmprestimoLimite){
+					Saldo += saldoTotal;
+				}
+			}
 		}
-		//Método de Saque
-		public void Saque(doublesaldoTotal)
-		{
-			Saldo -= saldoTotal;
-		}
-		//Método de Deposito
-		public void Deposito(doublesaldoTotal)
-		{
-			Saldo += saldoTotal;
-		}
-	}
 }
