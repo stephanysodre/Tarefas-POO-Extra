2e175cd48986550b0e7f3c9e4160268efdec0c52
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-11-16 10:39:24 -0300

    Update Class2.cs

--- Tarefa Herança/Class2.cs
+++ Tarefa Herança/Class2.cs
@@ -1,28 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Criado dentro do projeto uma pasta entidades para separar classes do programa principal
-namespace Tarefa_Herança
-{
-    //Criação da classe Conta
-    class ContaJuridica: Conta {
-        //Declarações das variáveis e uso do encapsulamento
-        public double EmprestimoLimite{ get; set; }
-//Criação do construtor padrão
-        public ContaJuridica() {}
-        //Criação do construtor com parâmetros utilizando o da Classe Conta
-        public ContaJuridica(intnumero, stringtitular, doublesaldo, doubleemprestimoLimite)
+namespace Course.Entities{
+class ContaPoupanca: Conta { 
+      public double TaxaJuros{ get; set; } 
+      public ContaPoupanca() {    } 
+      public ContaPoupanca
+        (int numero, string titular, double saldo, double taxaJuros)
         : base(numero, titular, saldo){
-EmprestimoLimite= emprestimoLimite;
-}
-public void Emprestimo(doublesaldoTotal){
-    if (saldoTotal <= EmprestimoLimite)
-    {
-        Saldo += saldoTotal;
-    }
-}
-}
+                       TaxaJuros = taxaJuros; 
+                       } 
+    public void atualizacaoSaldo(){ 
+                      Saldo += Saldo * taxaJuros; 
+                      } 
+     } 
+   }
 }
