42733dafd19a084005e4884245f45ae938bc9696
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-11-16 18:52:12 -0300

    Atividade Final

---
+++ AtividadeFinal_FolhadePagamento_180042/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeFinal_FolhadePagamento_180042
+{
+    class Class1
+    {
+    }
+}
